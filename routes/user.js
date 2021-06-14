5a8d45e4b56d803047762c76b844a778ffd30e90
Seb <sanner.sebastien@gmail.com>
2021-06-14 18:03:38 +0200

    :lock: add rate-limiter middleware

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,11 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
 
 const verifyPassword = require('../middleware/verifyPassword')
-const userCtrl = require('../controllers/user');
+const userCtrl = require('../controllers/user')
+const limiter = require('../middleware/rate-limiter')
 
-router.post('/signup', verifyPassword, userCtrl.signup);
-router.post('/login', userCtrl.login);
+router.post('/signup', verifyPassword, userCtrl.signup)
+router.post('/login', limiter, userCtrl.login)
 
-module.exports = router;
+module.exports = router
